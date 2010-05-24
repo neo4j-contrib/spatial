fedc8c9bc390298df474b0d74538f8107f82c56e
craig <craig@amanzi.com>
2010-05-24 15:52:07 +0200

    added comments and fixed import

--- src/main/java/org/neo4j/gis/spatial/GeometryUtils.java
+++ src/main/java/org/neo4j/gis/spatial/GeometryUtils.java
@@ -16,6 +16,8 @@
  */
 package org.neo4j.gis.spatial;
 
+import java.util.HashMap;
+
 import org.neo4j.graphdb.Node;
 
 import com.vividsolutions.jts.geom.Envelope;
@@ -25,64 +27,94 @@ import com.vividsolutions.jts.io.ParseException;
 import com.vividsolutions.jts.io.WKBReader;
 import com.vividsolutions.jts.io.WKBWriter;
 
-
 /**
- * @author Davide Savazzi 
+ * @author Davide Savazzi
  */
 public class GeometryUtils implements Constants {
 
-	// Public methods
-	
-	public static Envelope getEnvelope(Node node) {
-		double[] bbox = (double[]) node.getProperty(PROP_BBOX);
-			
-		// Envelope parameters: xmin, xmax, ymin, ymax)
-		return new Envelope(bbox[0], bbox[2], bbox[1], bbox[3]);
-	}	
-	
-	public static Envelope getEnvelope(Envelope e, Envelope e1) {
-		Envelope result = new Envelope(e);
-		result.expandToInclude(e1);
-		return result;
-	}
-	
-	public static void encode(Geometry geom, Node geomNode) {
-		geomNode.setProperty(PROP_TYPE, encodeGeometryType(geom.getGeometryType()));
+    // Public methods
 
-		Envelope mbb = geom.getEnvelopeInternal();				
-		geomNode.setProperty(PROP_BBOX, new double[] { mbb.getMinX(), mbb.getMinY(), mbb.getMaxX(), mbb.getMaxY() });					
+    /**
+     * Find and extract the bounding box from the node properties.
+     */
+    public static Envelope getEnvelope(Node node) {
+        double[] bbox = (double[])node.getProperty(PROP_BBOX);
 
-		WKBWriter writer = new WKBWriter();
-		geomNode.setProperty(PROP_WKB, writer.write(geom));
-	}
+        // Envelope parameters: xmin, xmax, ymin, ymax)
+        return new Envelope(bbox[0], bbox[2], bbox[1], bbox[3]);
+    }
 
-	public static Geometry decode(Node geomNode, GeometryFactory geomFactory) {
-		try {
-			WKBReader reader = new WKBReader(geomFactory);
-			return reader.read((byte[]) geomNode.getProperty(PROP_WKB));
-		} catch (ParseException e) {
-			throw new SpatialDatabaseException(e.getMessage(), e);
-		}
-	}
-	
+    /**
+     * Create a bounding box encompassing the two bounding boxes passed in.
+     * 
+     * @param e
+     * @param e1
+     * @return
+     */
+    public static Envelope getEnvelope(Envelope e, Envelope e1) {
+        Envelope result = new Envelope(e);
+        result.expandToInclude(e1);
+        return result;
+    }
 
-	// Private methods
-	
-	private static Integer encodeGeometryType(String jtsGeometryType) {
-		if ("Point".equals(jtsGeometryType)) {
-			return GTYPE_POINT;
-		} else if ("MultiPoint".equals(jtsGeometryType)) {
-			return GTYPE_MULTIPOINT;
-		} else if ("LineString".equals(jtsGeometryType)) {
-			return GTYPE_LINESTRING;
-		} else if ("MultiLineString".equals(jtsGeometryType)) {
-			return GTYPE_MULTILINESTRING;
-		} else if ("Polygon".equals(jtsGeometryType)) {
-			return GTYPE_POLYGON;
-		} else if ("MultiPolygon".equals(jtsGeometryType)) {
-			return GTYPE_MULTIPOLYGON;
-		} else {
-			throw new IllegalArgumentException("unknown type:" + jtsGeometryType);
-		}
-	}
+    /**
+     * Encode the geometry information into the Node. Currently this stores the geometry in WKB as a
+     * byte[] property of the Node. Future versions will allow alternative storage, like WKT,
+     * sub-graphs and vertex point arrays with additional metadata properties. TODO: Support more
+     * encoding options TODO: Optimize (reduce object creation)
+     * 
+     * @param geom
+     * @param geomNode
+     */
+    public static void encode(Geometry geom, Node geomNode) {
+        geomNode.setProperty(PROP_TYPE, encodeGeometryType(geom.getGeometryType()));
+
+        Envelope mbb = geom.getEnvelopeInternal();
+        geomNode.setProperty(PROP_BBOX, new double[] {mbb.getMinX(), mbb.getMinY(), mbb.getMaxX(), mbb.getMaxY()});
+
+        WKBWriter writer = new WKBWriter();
+        geomNode.setProperty(PROP_WKB, writer.write(geom));
+    }
+
+    /**
+     * Decode the geometry information in the Node, and create a JTS Geometry object. Currently this
+     * looks for a WKB byte[]. Future versions will also look for WKT, sub-graphs or other metadata
+     * options with different performance characteristics. TODO: Support more decoding options
+     * 
+     * @param geomNode
+     * @param geomFactory
+     * @return
+     */
+    public static Geometry decode(Node geomNode, GeometryFactory geomFactory) {
+        try {
+            WKBReader reader = new WKBReader(geomFactory);
+            return reader.read((byte[])geomNode.getProperty(PROP_WKB));
+        } catch (ParseException e) {
+            throw new SpatialDatabaseException(e.getMessage(), e);
+        }
+    }
+
+    // Private methods
+    private static HashMap<String, Integer> geometryTypesMap = new HashMap<String, Integer>();
+    static {
+        geometryTypesMap.put("Point", GTYPE_POINT);
+        geometryTypesMap.put("MultiPoint", GTYPE_MULTIPOINT);
+        geometryTypesMap.put("LineString", GTYPE_LINESTRING);
+        geometryTypesMap.put("MultiLineString", GTYPE_MULTILINESTRING);
+        geometryTypesMap.put("Polygon", GTYPE_POLYGON);
+        geometryTypesMap.put("MultiPolygon", GTYPE_MULTIPOLYGON);
+    }
+
+    private static Integer encodeGeometryType(String jtsGeometryType) {
+        // TODO: Consider alternatives for specifying type, like relationship to type category
+        // objects (or similar indexing structure)
+        Integer type = geometryTypesMap.get(jtsGeometryType);
+        if (type == null)
+            throw new IllegalArgumentException("Unknown JTS Geometry Type:" + jtsGeometryType);
+        return type;
+    }
+    private static long totalEncodeTime = 0;
+    private static long totalEncodeCount = 0;
+    private static long totalEncodeLength = 0;
+
 }
