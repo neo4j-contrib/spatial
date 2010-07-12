89a9bbbc28e76aa6814d37dba3852dd60dec0c74
Davide Savazzi <davide@davidesavazzi.net>
2010-07-12 22:45:23 +0200

    added getOrGuessGeometryType()

--- src/main/java/org/neo4j/gis/spatial/Layer.java
+++ src/main/java/org/neo4j/gis/spatial/Layer.java
@@ -156,6 +156,12 @@ public class Layer implements Constants, SpatialDataset {
 		}
 	}
 	
+	public Integer getOrGuessGeometryType() {
+		Integer geomType = getGeometryType();
+		if (geomType == null) geomType = guessGeometryType();
+		return geomType;
+	}
+	
 	public String[] getExtraPropertyNames() {
 		Node layerNode = getLayerNode();
 		if (layerNode.hasProperty(PROP_LAYERNODEEXTRAPROPS)) {
