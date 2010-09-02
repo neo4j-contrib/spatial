1fdfd2012cf2211d82c3f207472bbd8edc65a6a5
craig <craig.taverner@amanzitel.com>
2010-09-02 23:43:28 +0200

    now that dynamic layers boolean logic is corrected, test with many more examples

--- src/test/java/org/neo4j/gis/spatial/TestDynamicLayers.java
+++ src/test/java/org/neo4j/gis/spatial/TestDynamicLayers.java
@@ -17,32 +17,56 @@ public class TestDynamicLayers extends Neo4jTestCase {
 		// TODO: Consider merits of using dependency data in target/osm,
 		// downloaded by maven, as done in TestSpatial, versus the test data
 		// commited to source code as done here
-		loadTestOsmData("map.osm",1000);
+		loadTestOsmData("map.osm", 1000);
 
-		//Define dynamic layers
+		// Define dynamic layers
 		ArrayList<Layer> layers = new ArrayList<Layer>();
 		SpatialDatabaseService spatialService = new SpatialDatabaseService(graphDb());
-		OSMLayer osmLayer = (OSMLayer)spatialService.getLayer("map.osm");
-		HashMap<String,String> tags = new HashMap<String,String>();
-		tags.put("highway","residential");
-		layers.add(osmLayer.addDynamicLayerOnWayTags("highway.residential", Constants.GTYPE_LINESTRING, tags));
+		OSMLayer osmLayer = (OSMLayer) spatialService.getLayer("map.osm");
+		addSimpleDynamicLayer(osmLayer, layers, "highway", "primary");
+		addSimpleDynamicLayer(osmLayer, layers, "highway", "secondary");
+		addSimpleDynamicLayer(osmLayer, layers, "highway", "tertiary");
+		addSimpleDynamicLayer(osmLayer, layers, "highway", "residential");
+		addSimpleDynamicLayer(osmLayer, layers, "highway", "footway");
+		addSimpleDynamicLayer(osmLayer, layers, "highway", "cycleway");
+		addSimpleDynamicLayer(osmLayer, layers, "highway", "track");
+		addSimpleDynamicLayer(osmLayer, layers, "highway", "path");
+		addSimpleDynamicLayer(osmLayer, layers, "highway", "unclassified");
+		addSimpleDynamicLayer(osmLayer, layers, "amenity", "parking");
+		addSimpleDynamicLayer(osmLayer, layers, "railway", null);
+		addSimpleDynamicLayer(osmLayer, layers, "highway", null);
 
-		//Now export the layers to files
+		// Now export the layers to files
 		ShapefileExporter exporter = new ShapefileExporter(graphDb());
 		exporter.setExportDir("target/export");
-		for(Layer layer:layers){
+		for (Layer layer : layers) {
 			exporter.exportLayer(layer.getName());
 		}
 	}
 
-    private void loadTestOsmData(String layerName, int commitInterval) throws Exception {
-        String osmPath = layerName;
-        System.out.println("\n=== Loading layer " + layerName + " from " + osmPath + " ===");
-        reActivateDatabase(false, true, false);
-        OSMImporter importer = new OSMImporter(layerName);
-        importer.importFile(getBatchInserter(), osmPath);
-        reActivateDatabase(false, false, false);
-        importer.reIndex(graphDb(), commitInterval);
-    }
+	/**
+	 * Add a rule for a pure way based search, with a single property key/value
+	 * match on the way tags. All ways with the specified tag property will be
+	 * returned.
+	 * @param osmLayer
+	 * @param layers
+	 * @param key
+	 * @param value
+	 */
+	private void addSimpleDynamicLayer(OSMLayer osmLayer, ArrayList<Layer> layers, String key, String value) {
+		HashMap<String, String> tags = new HashMap<String, String>();
+		tags.put(key, value);
+		layers.add(osmLayer.addDynamicLayerOnWayTags(value==null ? key : key + "-" + value, Constants.GTYPE_LINESTRING, tags));
+	}
+
+	private void loadTestOsmData(String layerName, int commitInterval) throws Exception {
+		String osmPath = layerName;
+		System.out.println("\n=== Loading layer " + layerName + " from " + osmPath + " ===");
+		reActivateDatabase(false, true, false);
+		OSMImporter importer = new OSMImporter(layerName);
+		importer.importFile(getBatchInserter(), osmPath);
+		reActivateDatabase(false, false, false);
+		importer.reIndex(graphDb(), commitInterval);
+	}
 
 }
