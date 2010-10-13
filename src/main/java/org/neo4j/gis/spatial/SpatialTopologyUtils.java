040e201759c1fd6da8e64ff6de795db0df28ac21
Peter Neubauer <peter@neubauer.se>
2010-10-13 22:41:12 +0200

    snapping only to LinesStrings, so we can handle points in the same layer etc. not sure if that is right?

--- src/main/java/org/neo4j/gis/spatial/SpatialTopologyUtils.java
+++ src/main/java/org/neo4j/gis/spatial/SpatialTopologyUtils.java
@@ -11,6 +11,7 @@ import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Envelope;
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.LineString;
 import com.vividsolutions.jts.geom.Point;
 import com.vividsolutions.jts.linearref.LinearLocation;
 import com.vividsolutions.jts.linearref.LocationIndexedLine;
@@ -24,15 +25,19 @@ import com.vividsolutions.jts.linearref.LocationIndexedLine;
  */
 public class SpatialTopologyUtils {
 	/**
-	 * Inner class associating points and resulting geometry records to facilitate the result set returned.
+	 * Inner class associating points and resulting geometry records to
+	 * facilitate the result set returned.
+	 * 
 	 * @author craig
 	 */
-	public static class PointResult implements Map.Entry<Point,SpatialDatabaseRecord>, Comparable<PointResult> {
+	public static class PointResult implements
+			Map.Entry<Point, SpatialDatabaseRecord>, Comparable<PointResult> {
 		private Point point;
 		private SpatialDatabaseRecord record;
 		private double distance;
 
-		private PointResult(Point point, SpatialDatabaseRecord record, double distance) {
+		private PointResult(Point point, SpatialDatabaseRecord record,
+				double distance) {
 			this.point = point;
 			this.record = record;
 			this.distance = distance;
@@ -40,19 +45,19 @@ public class SpatialTopologyUtils {
 
 		public Point getKey() {
 			return point;
-        }
+		}
 
 		public SpatialDatabaseRecord getValue() {
-	        return record;
-        }
+			return record;
+		}
 
 		public double getDistance() {
 			return distance;
 		}
 
 		public SpatialDatabaseRecord setValue(SpatialDatabaseRecord value) {
-	        return this.record = value;
-        }
+			return this.record = value;
+		}
 
 		public int compareTo(PointResult other) {
 			if (this.distance == other.distance) {
@@ -63,51 +68,64 @@ public class SpatialTopologyUtils {
 				return 1;
 			}
 		}
-		
+
 		public String toString() {
-			return "Point[" + point + "] distance[" + distance + "] record[" + record + "]";
+			return "Point[" + point + "] distance[" + distance + "] record["
+					+ record + "]";
 		}
 	}
-	
-	public static ArrayList<PointResult> findClosestEdges(Point point, Layer layer) {
-		return findClosestEdges(point,layer,0.0);
+
+	public static ArrayList<PointResult> findClosestEdges(Point point,
+			Layer layer) {
+		return findClosestEdges(point, layer, 0.0);
 	}
-	public static ArrayList<PointResult> findClosestEdges(Point point, Layer layer, double distance) {
-		ReferencedEnvelope env = new ReferencedEnvelope(layer.getIndex().getLayerBoundingBox(),layer.getCoordinateReferenceSystem());
-		if(distance<=0.0) distance = env.getSpan(0) / 100.0;
+
+	public static ArrayList<PointResult> findClosestEdges(Point point,
+			Layer layer, double distance) {
+		ReferencedEnvelope env = new ReferencedEnvelope(layer.getIndex()
+				.getLayerBoundingBox(), layer.getCoordinateReferenceSystem());
+		if (distance <= 0.0)
+			distance = env.getSpan(0) / 100.0;
 		Envelope search = new Envelope(point.getCoordinate());
 		search.expandBy(distance);
 		GeometryFactory factory = layer.getGeometryFactory();
-		return findClosestEdges(point,layer,factory.toGeometry(search));
+		return findClosestEdges(point, layer, factory.toGeometry(search));
 	}
-	public static ArrayList<PointResult> findClosestEdges(Point point, Layer layer, Geometry filter) {
+
+	public static ArrayList<PointResult> findClosestEdges(Point point,
+			Layer layer, Geometry filter) {
 		ArrayList<PointResult> results = new ArrayList<PointResult>();
-        Search searchQuery = new SearchIntersect(filter);
-        layer.getIndex().executeSearch(searchQuery);
+		Search searchQuery = new SearchIntersect(filter);
+		layer.getIndex().executeSearch(searchQuery);
 		for (SpatialDatabaseRecord record : searchQuery.getResults()) {
 			Geometry geom = record.getGeometry();
-			LocationIndexedLine line = new LocationIndexedLine(geom);
-			LinearLocation here = line.project(point.getCoordinate());
-			Coordinate snap = line.extractPoint(here);
-			double distance = snap.distance(point.getCoordinate());
-			results.add(new PointResult(layer.getGeometryFactory().createPoint(snap),record,distance));
+			if (geom instanceof LineString) {
+				LocationIndexedLine line = new LocationIndexedLine(geom);
+				LinearLocation here = line.project(point.getCoordinate());
+				Coordinate snap = line.extractPoint(here);
+				double distance = snap.distance(point.getCoordinate());
+				results.add(new PointResult(layer.getGeometryFactory()
+						.createPoint(snap), record, distance));
+			}
 		}
 		Collections.sort(results);
 		return results;
 	}
 
-	public static ReferencedEnvelope scaleBounds(ReferencedEnvelope bounds, double factor) {
+	public static ReferencedEnvelope scaleBounds(ReferencedEnvelope bounds,
+			double factor) {
 		ReferencedEnvelope scaled = new ReferencedEnvelope(bounds);
 		if (Math.abs(factor - 1.0) > 0.01) {
 			double[] min = scaled.getLowerCorner().getCoordinate();
 			double[] max = scaled.getUpperCorner().getCoordinate();
-			for(int i=0;i<scaled.getDimension();i++){
+			for (int i = 0; i < scaled.getDimension(); i++) {
 				double span = scaled.getSpan(i);
 				double delta = (span - span * factor) / 2.0;
 				min[i] += delta;
 				max[i] -= delta;
 			}
-			scaled = new ReferencedEnvelope(min[0],max[0],min[1],max[1],scaled.getCoordinateReferenceSystem());
+			scaled = new ReferencedEnvelope(min[0], max[0], min[1], max[1],
+					scaled.getCoordinateReferenceSystem());
 		}
 		return scaled;
 	}
@@ -115,15 +133,15 @@ public class SpatialTopologyUtils {
 	public static Envelope scaleBounds(Envelope bounds, double factor) {
 		Envelope scaled = new Envelope(bounds);
 		if (Math.abs(factor - 1.0) > 0.01) {
-			double[] min = new double[]{scaled.getMinX(), scaled.getMinY()};
-			double[] max = new double[]{scaled.getMaxX(), scaled.getMaxY()};
-			for(int i=0;i<2;i++){
-				double span = (i==0) ? scaled.getWidth() : scaled.getHeight();
+			double[] min = new double[] { scaled.getMinX(), scaled.getMinY() };
+			double[] max = new double[] { scaled.getMaxX(), scaled.getMaxY() };
+			for (int i = 0; i < 2; i++) {
+				double span = (i == 0) ? scaled.getWidth() : scaled.getHeight();
 				double delta = (span - span * factor) / 2.0;
 				min[i] += delta;
 				max[i] -= delta;
 			}
-			scaled = new Envelope(min[0],max[0],min[1],max[1]);
+			scaled = new Envelope(min[0], max[0], min[1], max[1]);
 		}
 		return scaled;
 	}
