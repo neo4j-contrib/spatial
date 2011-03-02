ffede41ef9db127532c78cf1c7c8c96e8118484c
Craig Taverner <craig.taverner@amanzitel.com>
2011-03-02 11:24:32 +0100

    Removed compile errors from eclipse compile

--- src/main/java/org/neo4j/gis/spatial/indexprovider/LayerNodeIndex.java
+++ src/main/java/org/neo4j/gis/spatial/indexprovider/LayerNodeIndex.java
@@ -143,12 +143,10 @@ public class LayerNodeIndex implements Index<Node>
         return query(layerName, queryOrQueryObject);
     }
 
-    @Override
     public void remove(Node node, String s) {
         layer.delete(node.getId());
     }
 
-    @Override
     public void remove(Node node) {
         layer.delete(node.getId());
     }
