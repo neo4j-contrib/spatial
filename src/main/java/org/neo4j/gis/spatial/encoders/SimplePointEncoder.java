6fb92b8f491b40029d0095a2fc0c01b22d4fb43f
Peter Neubauer <peter@neubauer.se>
2011-08-08 09:30:55 +0200

    more robust encoding

--- src/main/java/org/neo4j/gis/spatial/encoders/SimplePointEncoder.java
+++ src/main/java/org/neo4j/gis/spatial/encoders/SimplePointEncoder.java
@@ -32,43 +32,51 @@ import com.vividsolutions.jts.geom.GeometryFactory;
  * 
  * @author craig
  */
-public class SimplePointEncoder extends AbstractGeometryEncoder implements Configurable {
-	protected GeometryFactory geometryFactory;
-	protected String xProperty = "longitude";
-	protected String yProperty = "latitude";
+public class SimplePointEncoder extends AbstractGeometryEncoder implements
+        Configurable
+{
+    protected GeometryFactory geometryFactory;
+    protected String xProperty = "longitude";
+    protected String yProperty = "latitude";
 
-	protected GeometryFactory getGeometryFactory() {
-		if (geometryFactory == null)
-			geometryFactory = new GeometryFactory();
-		return geometryFactory;
-	}
+    protected GeometryFactory getGeometryFactory()
+    {
+        if ( geometryFactory == null ) geometryFactory = new GeometryFactory();
+        return geometryFactory;
+    }
 
-	@Override
-	protected void encodeGeometryShape(Geometry geometry, PropertyContainer container) {
-		container.setProperty("gtype", SpatialDatabaseService.convertJtsClassToGeometryType(geometry.getClass()));
-		Coordinate[] coords = geometry.getCoordinates();
-		container.setProperty(xProperty, coords[0].x);
-		container.setProperty(yProperty, coords[0].y);
-	}
+    @Override
+    protected void encodeGeometryShape( Geometry geometry,
+            PropertyContainer container )
+    {
+        container.setProperty(
+                "gtype",
+                SpatialDatabaseService.convertJtsClassToGeometryType( geometry.getClass() ) );
+        Coordinate[] coords = geometry.getCoordinates();
+        container.setProperty( xProperty, coords[0].x );
+        container.setProperty( yProperty, coords[0].y );
+    }
 
-	public Geometry decodeGeometry(PropertyContainer container) {
-		double x = (Double) container.getProperty(xProperty);
-		double y = (Double) container.getProperty(yProperty);
-		Coordinate coordinate = new Coordinate(x, y);
-		return getGeometryFactory().createPoint(coordinate);
-	}
+    public Geometry decodeGeometry( PropertyContainer container )
+    {
+        double x = ( (Number) container.getProperty( xProperty ) ).doubleValue();
+        double y = ( (Number) container.getProperty( yProperty ) ).doubleValue();
+        Coordinate coordinate = new Coordinate( x, y );
+        return getGeometryFactory().createPoint( coordinate );
+    }
 
-	public String getConfiguration() {
-		return xProperty + ":" + yProperty;
-	}
+    public String getConfiguration()
+    {
+        return xProperty + ":" + yProperty;
+    }
 
-	public void setConfiguration(String configuration) {
-		if (configuration != null) {
-			String[] fields = configuration.split(":");
-			if (fields.length > 0)
-				xProperty = fields[0];
-			if (fields.length > 1)
-				yProperty = fields[1];
-		}
-	}
+    public void setConfiguration( String configuration )
+    {
+        if ( configuration != null )
+        {
+            String[] fields = configuration.split( ":" );
+            if ( fields.length > 0 ) xProperty = fields[0];
+            if ( fields.length > 1 ) yProperty = fields[1];
+        }
+    }
 }
