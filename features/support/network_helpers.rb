505b1a53d17ab27431d458d3134cf9b1b915d04b
Peter Neubauer <peter@neubauer.se>
2011-08-30 16:39:54 +0200

    reverting file copy

--- features/support/network_helpers.rb
+++ features/support/network_helpers.rb
@@ -23,14 +23,14 @@ module NetworkHelpers
 
   def copy_file(source, target)
     puts "copy '#{source}' -> '#{target}'"
-#    File.open(source, "r") do |src|
-#      open(target, "wb") do |file|
-#        while buf = src.read(2048)
-#          file.write(buf)
-#        end
-#      end
-#    end
-    File.cp(source, target) 
+    File.open(source, "r") do |src|
+      open(target, "wb") do |file|
+        while buf = src.read(2048)
+          file.write(buf)
+        end
+      end
+    end
+#    File.cp(source, target) 
   end
 
   def transfer_if_newer(location, target)
