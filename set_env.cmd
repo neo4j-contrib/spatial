73028181be1c4024a337d436cd399e946891b33d
peterneubauer <peter@neubauer.se>
2012-08-13 14:58:58 +0200

    removing old stuff

--- set_env.cmd
+++
@@ -1,6 +0,0 @@
-set NEO4J_HOME=target/neo4j_home
-set NEO4J_PRODUCT=community
-set NEO4J_VERSION=1.4-SNAPSHOT
-set DOWNLOAD_LOCATION=http://builder.neo4j.org/guestAuth/repository/download/bt65/.lastSuccessful/standalone/neo4j-community-1.4-SNAPSHOT-windows.zip
-set DOWNLOAD_PLUGIN_LOCATION=http://builder.neo4j.org/guestAuth/repository/download/bt32/.lastSuccessful/spatial/neo4j-spatial-0.6-SNAPSHOT.jar
-set DOWNLOAD_DEPENDENCIES_LOCATION=http://builder.neo4j.org/guestAuth/repository/download/bt32/.lastSuccessful/dependencies.zip
--- set_env.sh
+++
@@ -1,7 +0,0 @@
-export NEO4J_HOME=neo4j_home
-export NEO4J_PRODUCT=community
-export NEO4J_VERSION=1.5-SNAPSHOT
-#export DOWNLOAD_LOCATION=http://builder.neo4j.org/guestAuth/repository/download/bt65/.lastSuccessful/standalone/neo4j-community-1.5-SNAPSHOT-windows.zip
-export DOWNLOAD_LOCATION=http://builder.neo4j.org/guestAuth/repository/download/bt65/.lastSuccessful/standalone/neo4j-community-1.5-SNAPSHOT-unix.tar.gz
-#export DOWNLOAD_LOCATION=/Users/peterneubauer/code/neo/build/packaging/standalone/target/neo4j-community-1.5-SNAPSHOT-unix.tar.gz
-export DOWNLOAD_PLUGIN_LOCATION=file:`pwd`/target/neo4j-spatial-0.7-SNAPSHOT-server-plugin.zip
