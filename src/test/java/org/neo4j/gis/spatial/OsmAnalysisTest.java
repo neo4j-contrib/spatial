5ed78dddf4a53864f12cd88d8925681ad6a99eb0
Craig Taverner <craig@amanzi.com>
2013-02-01 23:31:45 +0100

    Moved SLD files to subdir

--- src/test/java/org/neo4j/gis/spatial/OsmAnalysisTest.java
+++ src/test/java/org/neo4j/gis/spatial/OsmAnalysisTest.java
@@ -223,7 +223,7 @@ public class OsmAnalysisTest extends TestOSMImport {
 		imageExporter.setZoom(2.0);
 		imageExporter.setOffset(-0.2, 0.25);
 		imageExporter.setSize(1280, 800);
-		imageExporter.setStyleFiles(new String[] { "background.sld", "rank.sld" });
+		imageExporter.setStyleFiles(new String[] { "sld/background.sld", "sld/rank.sld" });
 
 		String[] layerPropertyNames = new String[]{"name", "timestamp", "user", "days", "user_rank"};
 		StringBuffer userParams = new StringBuffer();
