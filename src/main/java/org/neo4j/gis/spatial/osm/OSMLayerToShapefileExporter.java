911aa874c4892824c9a2652736e16da568f2d504
Craig Taverner <craig@amanzi.com>
2021-05-10 10:50:35 +0200

    Fixed copyright headers to new text used in Neo4j (removed date range)

--- src/main/java/org/neo4j/gis/spatial/osm/OSMLayerToShapefileExporter.java
+++ src/main/java/org/neo4j/gis/spatial/osm/OSMLayerToShapefileExporter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2020 "Neo4j,"
+ * Copyright (c) "Neo4j"
  * Neo4j Sweden AB [http://neo4j.com]
  *
  * This file is part of Neo4j Spatial.
@@ -15,7 +15,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.neo4j.gis.spatial.osm;
 
